73f77439266ce92fd0f28a06c0ff367c9b02de76
Faisal Moeed <faisal.moeed@ontario.ca>
2026-04-10 10:53:54 -0400

    code updated

--- oid4vc/auth_server/docker/entrypoint.sh
+++ oid4vc/auth_server/docker/entrypoint.sh
@@ -41,8 +41,6 @@ echo "TENANT_ISSUER_BASE_URL: $TENANT_ISSUER_BASE_URL"
 
 # Run Alembic migrations
 echo $PWD
-#pip install poetry
-echo "PATH: $PATH"
 
 poetry run python alembic/admin/migrate.py
 
